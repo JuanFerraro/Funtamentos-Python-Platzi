86b7455173633874238552403c9bc9f2eeff3385
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-10 11:12:25 -0500

    Clases 1 y 2 (variables y print)

---
+++ 02_vars.py
@@ -0,0 +1,11 @@
+print("Hola, variables")
+
+#Creacion de variable en python:
+mi_nombre = "Juan Barrios"
+
+#Imprimir variable:
+print("Bienvenido " + mi_nombre)
+
+#Variable con numero:
+mi_edad = 26
+print(mi_edad)
---
+++ main.py
@@ -0,0 +1,12 @@
+print("Primea linea de código")
+
+print("Juaan aprendiendo python")
+
+#Operadores
+print(12 + 5)
+print(12 - 5)
+print(12 * 5)
+print(12 / 5)
+
+#Documentacion del codigo
+#Fundamental para anotaciones
